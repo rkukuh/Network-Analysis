d05b9580aa3fd0db5288099dbbb8e87d3e1f053e
R. Kukuh <rkukuh@gmail.com>
2021-11-15 11:50:04 +0700

    Initial Commit

---
+++ Network Analysis/Network_AnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Network_AnalysisApp.swift
+//  Network Analysis
+//
+//  Created by R. Kukuh on 15/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Network_AnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
