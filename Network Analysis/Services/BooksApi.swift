3c61a8aba5b8290e6b5559267fbbadf6452128d9
R. Kukuh <rkukuh@gmail.com>
2021-11-15 12:02:38 +0700

    Create service to fetch book API data

--- Network Analysis/Services/BooksApi.swift
+++ Network Analysis/Services/BooksApi.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class BooksApi: ObservableObject {
+    @Published var books = [Book]()
+    
+    func loadData(completion:@escaping ([Book]) -> ()) {
+        let booksApiURL = "https://training.xcelvations.com/data/books.json"
+        
+        guard let url = URL(string: booksApiURL) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            let books = try! JSONDecoder().decode([Book].self, from: data!)
+            
+            DispatchQueue.main.async {
+                completion(books)
+            }
+        }.resume()
+    }
+}
