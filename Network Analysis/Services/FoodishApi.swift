157a3b6016dc8758edaf2b3dd056c1e0b70399b4
R. Kukuh <rkukuh@gmail.com>
2021-11-16 11:18:55 +0700

    Create service to fetch API data

--- Network Analysis/Services/FoodishApi.swift
+++ Network Analysis/Services/FoodishApi.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+class FoodishApi: ObservableObject {
+    @Published var food = Food(image: "")
+    
+    func getData(completion: @escaping (Food) -> ()) {
+        let urlString = "https://foodish-api.herokuapp.com/api/"
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let data = data {
+                do {
+                    let food = try JSONDecoder().decode(Food.self, from: data)
+                    
+                    print(food.image)
+                    
+                    DispatchQueue.main.async {
+                        completion(food)
+                    }
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }.resume()
+    }
+}
