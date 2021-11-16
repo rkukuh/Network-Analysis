918632250d4ac7274fc815f4f276a51047227c4d
R. Kukuh <rkukuh@gmail.com>
2021-11-16 15:07:08 +0700

    Create API service to fetch meal data

--- Network Analysis/Services/MealDbApi.swift
+++ Network Analysis/Services/MealDbApi.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+class MealDbApi: ObservableObject {
+    @Published var meals = [Meal]()
+    
+    func loadData(completion: @escaping ([Meal]) -> ()) {
+        let mealDbApiURL = "www.themealdb.com/api/json/v1/1/filter.php?a=Japanese"
+        
+        guard let url = URL(string: mealDbApiURL) else { return }
+        
+        let session = URLSession(configuration: .default)
+        
+        session.sessionDescription = "Main Session"
+        
+        let task = session.dataTask(with: url) { data, response, error in
+            let meals = try! JSONDecoder().decode([Meal].self, from: data!)
+            
+            print(meals)
+            
+            DispatchQueue.main.async {
+                completion(meals)
+            }
+        }
+        
+        task.taskDescription = "Get meals filtered by area"
+        task.resume()
+    }
+}
